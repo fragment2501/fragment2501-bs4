0e81f75fabd7296f3c0601000634380e84bb73bd
Bryan <fragment2501@users.noreply.github.com>
2019-11-04 16:36:58 -0500

    BS4 card layout for blog. Removed custom styles in layout in favor of BS4 grid/layout classes. (#1)

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <div id="content-wrapper">
+    <div class="container">
       <Header/>
       <slot/>
     </div>
@@ -22,27 +22,5 @@ export default {
 
 
 <style lang="scss">
-body, html {
-  height: 100%;
-}
 
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-#app {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-}
-
-#content-wrapper {
-  flex: 1 0 auto;
-  max-width: 950px;
-  width: 90vw;
-  margin: 0 auto;
-}
 </style>
