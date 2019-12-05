d0f65c9268912f020efd41ff49e5f3654cae9d89
Bryan <fragment2501@users.noreply.github.com>
2019-12-05 15:24:41 -0800

    Updated the main js file which has all the goo to wire up the now-ui-kit components so they can be used in layouts and pages.

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import './assets/styles/main.scss'
 
 import checkIfMobile from './mixins/checkIfMobile'
 
+import globalDirectives from './plugins/globalDirectives';
+import globalMixins from './plugins/globalMixins';
+import globalComponents from './plugins/globalComponents';
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+locale.use(lang);
+
 export default function (Vue, { router, head, isClient }) {
   Vue.use(BootstrapVue)
   Vue.component('Layout', DefaultLayout)
   Vue.mixin(checkIfMobile)
+  Vue.use(globalDirectives);
+  Vue.use(globalMixins);
+  Vue.use(globalComponents);
 }
