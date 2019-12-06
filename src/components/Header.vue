2c327ae5266b438e96637884d811838028ac6ffd
Bryan <fragment2501@users.noreply.github.com>
2019-12-06 09:54:49 -0500

    Add now-ui-kit vue template so the site looks more sexy. (#3)
    * BS4 card layout for blog. Removed custom styles in layout in favor of BS4 grid/layout classes.
    
    * Added dark theme styles. Added code block formatting.
    
    * Fixed to be more responsive using row/col classes.
    
    * Added now-ui-kit styles and fonts.
    
    * Added all the base now-ui-kit vue components that I think we need to get the core starter kit working.
    
    * Updated the main js file which has all the goo to wire up the now-ui-kit components so they can be used in layouts and pages.
    
    * Updated the existing header and footer components from the old BS4 template with the new now-ui-kit markup and components.  Updated the default layout to use them and to pass along the props needed to manage the navbar settings.  Updated the Index page to use the landing page template from now-ui-kit.
    
    * Added required package element-ui
    
    * Added the othe vue2-transitions dep for now ui kit.
    
    * Added now-ui-kit class to enable the hamberger menu and sidebar.
    
    * added some custom images to the landing page.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,128 @@
+
 <template>
-  <header class="header">
-    <b-navbar class="p-0" toggleable="md" type="light">
-        <b-navbar-brand>
-        <g-link class="logo-home logo-desktop" to="/">{{ $static.metadata.siteName }}</g-link>
-        <g-link class="logo-home logo–mobile" to="/"><img class="logo-image" alt="logo" src="../assets/images/logo.png"></g-link>
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav class="ml-auto">
-          <g-link class="nav--link" to="/about">About</g-link>
-          <g-link class="nav--link" to="/blog">Blog</g-link>
-          <g-link class="nav--link" to="/contact">Contact</g-link>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </header>
+  <navbar
+    position="fixed"
+    type="primary"
+    :transparent="transparent"
+    :color-on-scroll="colorOnScroll"
+    menu-classes="ml-auto"
+  >
+    <template slot-scope="{ toggle, isToggled }">
+      <router-link v-popover:popover1 class="navbar-brand" to="/">
+        Now Ui Kit
+      </router-link>
+      <el-popover
+        ref="popover1"
+        popper-class="popover"
+        placement="bottom"
+        width="200"
+        trigger="hover"
+      >
+        <div class="popover-body">
+          Designed by Invision. Coded by Creative Tim
+        </div>
+      </el-popover>
+    </template>
+    <template slot="navbar-menu">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="https://www.creative-tim.com/product/vue-now-ui-kit"
+          target="_blank"
+        >
+          <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+          <p>Download</p>
+        </a>
+      </li>
+      <drop-down
+        tag="li"
+        title="Components"
+        icon="now-ui-icons design_app"
+        class="nav-item"
+      >
+        <nav-link to="/">
+          <i class="now-ui-icons business_chart-pie-36"></i> All components
+        </nav-link>
+        <a
+          href="https://demos.creative-tim.com/vue-now-ui-kit/documentation"
+          target="_blank"
+          class="dropdown-item"
+        >
+          <i class="now-ui-icons design_bullet-list-67"></i> Documentation
+        </a>
+      </drop-down>
+      <drop-down
+              tag="li"
+              title="Examples"
+              icon="now-ui-icons design_image"
+              class="nav-item"
+      >
+        <nav-link to="/landing">
+          <i class="now-ui-icons education_paper"></i> Landing
+        </nav-link>
+        <nav-link to="/login">
+          <i class="now-ui-icons users_circle-08"></i> Login
+        </nav-link>
+        <nav-link to="/profile">
+          <i class="now-ui-icons users_single-02"></i> Profile
+        </nav-link>
+      </drop-down>
+      <li class="nav-item">
+        <a
+          class="nav-link btn btn-neutral"
+          href="https://www.creative-tim.com/product/vue-now-ui-kit-pro"
+          target="_blank"
+        >
+          <i class="now-ui-icons arrows-1_share-66"></i>
+          <p>Upgrade to PRO</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Follow us on Twitter"
+          data-placement="bottom"
+          href="https://twitter.com/CreativeTim"
+          target="_blank"
+        >
+          <i class="fab fa-twitter"></i>
+          <p class="d-lg-none d-xl-none">Twitter</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Like us on Facebook"
+          data-placement="bottom"
+          href="https://www.facebook.com/CreativeTim"
+          target="_blank"
+        >
+          <i class="fab fa-facebook-square"></i>
+          <p class="d-lg-none d-xl-none">Facebook</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Follow us on Instagram"
+          data-placement="bottom"
+          href="https://www.instagram.com/CreativeTimOfficial"
+          target="_blank"
+        >
+          <i class="fab fa-instagram"></i>
+          <p class="d-lg-none d-xl-none">Instagram</p>
+        </a>
+      </li>
+    </template>
+  </navbar>
 </template>
+
+
+
 
 <static-query>
 query {
@@ -25,59 +132,27 @@ query {
 }
 </static-query>
 
+
+<script>
+import { DropDown, NavbarToggleButton, Navbar, NavLink } from '@/components';
+import { Popover } from 'element-ui';
+
+export default {
+  name: 'main-navbar',
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number
+  },
+  components: {
+    DropDown,
+    Navbar,
+    NavbarToggleButton,
+    NavLink,
+    [Popover.name]: Popover
+  }
+};
+</script>
+
 <style lang="scss">
-nav {
-  width: 100%;
-  padding-top: 1rem;
-}
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-}
-
-.logo-home {
-  font-size: 24px;
-  max-width: 75%;
-  font-weight: bold;
-  &:hover {
-    text-decoration: none;
-  }
-}
-
-.logo-desktop {
-  @media(max-width: 419px) {
-    display: none;
-  }
-  @media (min-width: 420px) {
-    display: default;
-  }
-}
-
-.logo–mobile {
-  @media(max-width: 419px) {
-    display: default;
-  }
-  @media (min-width: 420px) {
-    display: none;
-  }
-}
-
-.logo-image {
-  width: 40px;
-}
-
-.nav--link {
-  margin-left: 20px;
-  @media (max-width: 419px) {
-    margin: 0;
-    padding: 15px 10px 10px 0;
-  }
-}
-
-.active--exact {
-  font-weight: bold;
-}
 </style>
