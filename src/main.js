5c1ee284746d6325bf003a40e22cb4b7c4a1df6c
Bryan <fragment2501@users.noreply.github.com>
2019-12-17 13:12:47 -0500

    feat: Add FontAwesome icons (#8)
    * Added fa packages.
    
    * Implementing FA tags on the page as described in the gridsome docs.
    
    * Removed the employee bio section, it has fontawesome icons but that whole section can go.
    
    * Replacing more tempalte words with more silly words.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
+// Default bootstrap stuff.
 import BootstrapVue from 'bootstrap-vue'
+
+// Our custom CSS stuff (Including the CSS stuff ror vue-now-kit).
 import './assets/styles/main.scss'
 
+// This is a carryover from the orignal gridsome template, we can probably remove this.
 import checkIfMobile from './mixins/checkIfMobile'
 
+// Creative Tim vue-now-kit stuff.
 import globalDirectives from './plugins/globalDirectives';
 import globalMixins from './plugins/globalMixins';
 import globalComponents from './plugins/globalComponents';
@@ -12,11 +17,24 @@ import lang from 'element-ui/lib/locale/lang/en';
 import locale from 'element-ui/lib/locale';
 locale.use(lang);
 
+// Font Awesome Icons
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faTwitter, faGooglePlusSquare, faSlack } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false;
+library.add(faGithub, faTwitter, faGooglePlusSquare, faSlack);
+
 export default function (Vue, { router, head, isClient }) {
+  // Default bootstrap stuff.
   Vue.use(BootstrapVue)
   Vue.component('Layout', DefaultLayout)
+  // we can probably safely remove this mobile check.
   Vue.mixin(checkIfMobile)
+  // Creative Tim vue-now-kit stuff. 
   Vue.use(globalDirectives);
   Vue.use(globalMixins);
   Vue.use(globalComponents);
+  // Font awesome stuff.
+  Vue.component('font-awesome', FontAwesomeIcon);
 }
