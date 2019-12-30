5c9a514f714c02885ed174c28b8a408b07fae0e9
Bryan <fragment2501@users.noreply.github.com>
2019-12-30 14:41:48 -0800

    Added orange image banners to the blog and contact pages.

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,39 +1,40 @@
 <template>
   <Layout :colorOnScroll="400">
 
-  <div class="page-header page-header-small">
-    <parallax
-      class="page-header-image"
-      style="background-image: url('/images/barbg.jpg')"
-    >
-    </parallax>
+  <div class="profile-page">
+    <div filter-color="orange" class="page-header page-header-small clear-filter">
+      <parallax
+        class="page-header-image"
+        style="background-image: url('/images/mt_bg1.jpg')"
+      >
+      </parallax>
 
-    <div class="content-center">
-      <div class="container">
-        <h1 class="title">This is a blog listing! Yay!</h1>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title">This is a blog listing! Yay!</h1>
+        </div>
       </div>
     </div>
-  </div>
 
-  <br/>
+    <br/>
 
-    <div class="row justify-content-center"
-      v-for="item in $page.posts.edges"
-      :key="item.node.id"
-    >
+      <div class="row justify-content-center"
+        v-for="item in $page.posts.edges"
+        :key="item.node.id"
+      >
 
-      <div class="col-md-8 col-lg-6 mb-3">
-        <div class="card" >
-          <g-image v-if="item.node.cover_image" :src="item.node.cover_image" alt="image" class="card-img-top"  />
-          <div class="card-body">
-            <h5 class="card-title">{{item.node.title}}</h5>
-            <p class="card-text">{{item.node.description}}</p>
-            <g-link :to="item.node.path" class="btn btn-primary">Continue Reading</g-link>
+        <div class="col-md-8 col-lg-6 mb-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">{{item.node.title}}</h5>
+              <p class="card-text">{{item.node.description}}</p>
+              <g-link :to="item.node.path" class="btn btn-primary">Continue Reading</g-link>
+            </div>
           </div>
         </div>
       </div>
 
-</div>
+    </div>
   </Layout>
 </template>
 
