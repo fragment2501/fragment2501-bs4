4bf8a604444489282d65abb381d810c0260d9603
Bryan <fragment2501@users.noreply.github.com>
2019-12-29 21:35:09 -0500

    Updates to header/footer/homepage. Fixed layout for contact page and added to nav. (#20)
    * Reduce the card size for the blog listing page on large screens.
    
    * Updated links in the footer to not be the default creative tim stuff.
    
    * Changed external links back to a tags since the g tag stuff only helps for links to vue app pages.
    
    * Updated links to twitter and github.
    
    * Whitespace/indentation
    
    * Moved contact info from index page. Updated the contact page layout to look more like the other pages.  Added contact link to header/footer. Removed home link from header (the header site icon is the home link).
    
    * Updated favicon.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,9 +11,8 @@
           <div class="container">
             <h1 class="title">This is a landing page!</h1>
             <div class="text-center">
-              <a href="#fragment2501" class="btn btn-primary btn-icon btn-round"><i class="fab"><font-awesome :icon="['fab', 'slack']"/></i></a>
-              <a href="#fragment2501" class="btn btn-primary btn-icon btn-round"><i class="fab"><font-awesome :icon="['fab', 'twitter']"/></i></a>
-              <a href="#fragment2501" class="btn btn-primary btn-icon btn-round"><i class="fab"><font-awesome :icon="['fab', 'google-plus-square']"/></i></a>
+              <a href="https://twitter.com/Fragment2501" class="btn btn-primary btn-icon btn-round" target="_blank" rel="noopener"><i class="fab"><font-awesome :icon="['fab', 'twitter']"/></i></a>
+              <a href="https://github.com/fragment2501" class="btn btn-primary btn-icon btn-round" target="_blank" rel="noopener"><i class="fab"><font-awesome :icon="['fab', 'github']"/></i></a>
             </div>
           </div>
         </div>
@@ -91,45 +90,6 @@
                   year-by-year, for all kinds of natural reasons, there’s huge
                   variety of the state of the ice.
                 </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="section section-contact-us text-center">
-        <div class="container">
-          <h2 class="title">Send me a message!</h2>
-          <p class="description">Bored out of your mind waiting for that Pokemon Go raid to start? Still angry about how bad Hollow Man was?  Message me about it using this silly non-instant message delivery system! Hooray!</p>
-          <div class="row">
-            <div class="col-lg-6 text-center ml-auto mr-auto col-md-8">
-              <fg-input
-                class="input-lg"
-                placeholder="Your Name..."
-                v-model="form.firstName"
-                addon-left-icon="now-ui-icons users_circle-08"
-              >
-              </fg-input>
-              <fg-input
-                class="input-lg"
-                placeholder="Email Here..."
-                v-model="form.email"
-                addon-left-icon="now-ui-icons ui-1_email-85"
-              >
-              </fg-input>
-              <div class="textarea-container">
-                <textarea
-                  class="form-control"
-                  name="name"
-                  rows="4"
-                  cols="80"
-                  v-model="form.message"
-                  placeholder="Type a message..."
-                ></textarea>
-              </div>
-              <div class="send-button">
-                <n-button type="primary" round block size="lg"
-                  >Send Message</n-button
-                >
               </div>
             </div>
           </div>
