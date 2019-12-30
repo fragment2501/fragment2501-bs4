e4f1dc12856f26b130b5fd91443173854dc93c35
Bryan <fragment2501@users.noreply.github.com>
2019-12-30 12:25:21 -0800

    Minor tweak to the col size for blog posts to try to help with images.

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -1,21 +1,25 @@
 <template>
   <Layout>
     <div class="container mt-5 pt-5">
-    <g-image v-if="$page.post.cover_image" class="blogImage mb-4" :src="$page.post.cover_image" />
-    <div class="blogPost">
-      <h1 v-html="$page.post.title" class="mb-4"/>
-      <div class="meta">
-        <div class="box date">
-          <span class="label">Date</span>
-          <div v-text="new Date($page.post.date).toLocaleDateString()"/>
-        </div>
-        <div class="box time">
-          <span class="label">Time</span>
-          <span>{{ $page.post.timeToRead }} min read</span>
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <g-image v-if="$page.post.cover_image" class="blogImage mb-4" :src="$page.post.cover_image" />
+          <div class="blogPost">
+            <h1 v-html="$page.post.title" class="mb-4"/>
+            <div class="meta">
+              <div class="box date">
+                <span class="label">Date</span>
+                <div v-text="new Date($page.post.date).toLocaleDateString()"/>
+              </div>
+              <div class="box time">
+                <span class="label">Time</span>
+                <span>{{ $page.post.timeToRead }} min read</span>
+              </div>
+            </div>
+            <BlogContent class="mt-5" :content="$page.post.content"/>
+          </div>
         </div>
       </div>
-      <BlogContent class="mt-5" :content="$page.post.content"/>
-    </div>
     </div>
   </Layout>
 </template>
