10d8b9b3aae96bad2b83dfabfd56a85bf27c53a6
Bryan <fragment2501@users.noreply.github.com>
2020-01-02 11:36:53 -0500

    Added orange image banners to the blog and contact pages. (#24)

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,79 +1,81 @@
 <template>
   <Layout :colorOnScroll="400">
 
-  <div class="page-header page-header-small">
-    <parallax
-      class="page-header-image"
-      style="background-image: url('/images/barbg.jpg')"
-    >
-    </parallax>
+  <div class="profile-page">
+    <div filter-color="orange" class="page-header page-header-small clear-filter">
+      <parallax
+        class="page-header-image"
+        style="background-image: url('/images/mt.jpg')"
+      >
+      </parallax>
 
-    <div class="content-center">
-      <div class="container">
-        <h1 class="title">Send me a message!</h1>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title">Send me a message!</h1>
+        </div>
       </div>
     </div>
-  </div>
 
-  <br/>
+    <br/>
 
-  <div class="row justify-content-center">
-    <div class="col-md-8 col-lg-6 mb-3">
-        <p class="description">
-          Bored out of your mind waiting for that Pokemon Go raid to start? Still angry about how bad Hollow Man was?  Message me about it using this silly non-instant message delivery system! Hooray!
-        </p>
-        <div>
-          <b-form 
-            name="contact"
-            method="post"
-            @submit.prevent="handleSubmit"
-            action="/success/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-          <input type="hidden" name="name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" />
-            </label>
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6 mb-3">
+          <p class="description">
+            Bored out of your mind waiting for that Pokemon Go raid to start? Still angry about how bad Hollow Man was?  Message me about it using this silly non-instant message delivery system! Hooray!
           </p>
-            <b-form-group id="input-group-2" label="Name:" label-for="form-name">
-              <b-form-input
-                id="name"
-                name="name"
-                v-model="form.name"
-                required
-                placeholder="Enter name"
-              />
-            </b-form-group>
-
-            <b-form-group
-              id="input-group-1"
-              label="Email:"
-              label-for="email"
+          <div>
+            <b-form 
+              name="contact"
+              method="post"
+              @submit.prevent="handleSubmit"
+              action="/success/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
             >
-              <b-form-input
-                id="email"
-                v-model="form.email"
-                name="email"
-                type="email"
-                required
-                placeholder="Enter email"
-              />
-            </b-form-group>
+            <input type="hidden" name="name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out: <input name="bot-field" />
+              </label>
+            </p>
+              <b-form-group id="input-group-2" label="Name:" label-for="form-name">
+                <b-form-input
+                  id="name"
+                  name="name"
+                  v-model="form.name"
+                  required
+                  placeholder="Enter name"
+                />
+              </b-form-group>
 
-            <b-form-group id="input-group-3" label="Message:" label-for="message">
-              <b-form-textarea
-                id="message"
-                name="message"
-                v-model="form.message"
-                required
-                placeholder="Enter message"
-              />
-            </b-form-group>
+              <b-form-group
+                id="input-group-1"
+                label="Email:"
+                label-for="email"
+              >
+                <b-form-input
+                  id="email"
+                  v-model="form.email"
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Enter email"
+                />
+              </b-form-group>
 
-            <b-button type="submit" variant="primary">Submit</b-button>
-          </b-form>
+              <b-form-group id="input-group-3" label="Message:" label-for="message">
+                <b-form-textarea
+                  id="message"
+                  name="message"
+                  v-model="form.message"
+                  required
+                  placeholder="Enter message"
+                />
+              </b-form-group>
+
+              <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+          </div>
         </div>
       </div>
     </div>
