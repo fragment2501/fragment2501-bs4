d913051fce8a55ce734f84164c3a60d86d3085d8
Bryan <fragment2501@users.noreply.github.com>
2020-01-02 11:36:53 -0500

    refactor: Added orange image banners to the blog and contact pages. (#24)

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,31 +1,33 @@
 <template>
   <Layout :colorOnScroll="400">
 
-    <div class="page-header page-header-small">
-      <parallax
-        class="page-header-image"
-        style="background-image: url('/images/barbg.jpg')"
-      >
-      </parallax>
+    <div class="profile-page">
+      <div filter-color="orange" class="page-header page-header-small clear-filter">
+        <parallax
+          class="page-header-image"
+          style="background-image: url('/images/mt_bg1.jpg')"
+        >
+        </parallax>
 
-      <div class="content-center">
-        <div class="container">
-          <h1 class="title">This is a blog listing! Yay!</h1>
+        <div class="content-center">
+          <div class="container">
+            <h1 class="title">This is a blog listing! Yay!</h1>
+          </div>
         </div>
       </div>
-    </div>
 
-    <br/>
+      <br/>
 
-    <div class="container">
-      <div class="card" 
-        v-for="item in $page.posts.edges"
-        :key="item.node.id"
-      >
-        <div class="card-body">
-          <h2 class="card-title">{{item.node.title}}</h2>
-          <p class="card-text">{{item.node.description}}</p>
-          <g-link :to="item.node.path" class="btn btn-primary">Continue Reading</g-link>
+      <div class="container">
+        <div class="card" 
+          v-for="item in $page.posts.edges"
+          :key="item.node.id"
+        >
+          <div class="card-body">
+            <h2 class="card-title">{{item.node.title}}</h2>
+            <p class="card-text">{{item.node.description}}</p>
+            <g-link :to="item.node.path" class="btn btn-primary">Continue Reading</g-link>
+          </div>
         </div>
       </div>
     </div>
