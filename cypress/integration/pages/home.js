4b1fc5175ef9588f22e1e181630d8be78731dd78
Bryan <fragment2501@users.noreply.github.com>
2020-02-04 21:11:24 -0800

    feat: Add employee bio widget with backend example content. (#36)
    * Images for the bio widget section.
    
    * Markup for the about us section.
    
    * Added some simple tests for this new team/bio section.

--- cypress/integration/pages/home.js
+++ cypress/integration/pages/home.js
@@ -1,8 +1,21 @@
 describe('The Home Page', function()
 {
   it('Visit the home page and perform page specific tests.', function(){
+    // Goto the home page.
     cy.visit('/');
-    cy.get('h1');
+
+    // Tests for the page header stuff.
+    cy.get('h1').should('contain','Bryan\'s Blog!');
+
+    // Tests for the main about section.
+    cy.get('.section-about-us h2').should('contain','My little blog R&D site');
+
+    // Tests for the lower team section.
+    cy.get('.section-team h2').should('contain','Some Cool CMS Backend Options');
+    // confirm there are 3 rows of team section stuff
+    cy.get('.section-team .team .row .team-player').should('have.length',3);
+
+    // Percy screenshot for visual diffs.
     cy.percySnapshot('Home Page');
   });
 });
