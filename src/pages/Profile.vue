b6014cba5ee358b3988b14d2f03c6212777ee08a
Bryan <fragment2501@users.noreply.github.com>
2020-09-15 14:09:50 -0400

    feat: more work on get/set async calls. Moved profile api form to theme form controls.

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -7,21 +7,43 @@
       </div>
       <div v-if="auth && auth.isAuthenticated">
 
+        <h2>Backend API with OAuth Get/Set Example</h2>
+        <p> This section gets a bearer token from the OAuth browser session and then sends ajax requests to the backend api along with the bearer token for oauth validation on the backend.</p>
+
+        <div class="form-group">
+          <label for="profile-id" >ID</label>
+          <input v-model="profile._id" type="text" class="form-control" id="profile-id" disabled />
+        </div>
+
+        <div class="form-group">
+          <label for="profile-name" >Name</label>
+          <input v-model="profile.name" type="text" class="form-control" id="profile-name" placeholder="Enter your name" />
+        </div>
+
+        <div class="form-group">
+          <label for="profile-age" >Age</label>
+          <input v-model="profile.age" type="text" class="form-control" id="profile-age" placeholder="Enter your age">
+        </div>
+
         <div>
-          API MESSAGE: {{ apiMessage }}
-          <br/>
-          <button v-on:click="callApi">call api</button>
+          <button v-on:click="getProfile" class="btn btn-default">Refresh</button>
+          <button v-on:click="setProfile" class="btn btn-primary">Save</button>
+        </div>
+
+<br/><br/>
+
+        <div>
+          <h2>Client OAuth token</h2>
+          <p>This is a dump of the oauth token from your browser.</p>
+          <pre>{{ JSON.stringify(auth.user, null, 2) }}</pre>
         </div>
 
         <div>
           <img :src="auth.user.picture">
-          <h2>{{ auth.user.name }}</h2>
+          <p>{{ auth.user.name }}</p>
           <p>{{ auth.user.email }}</p>
         </div>
 
-        <div>
-          <pre>{{ JSON.stringify(auth.user, null, 2) }}</pre>
-        </div>
       </div>
     </div>
   </Layout>
@@ -34,20 +56,50 @@ export default{
   mounted() {
     this.auth = this.$auth || {};
   },
-  data(){ return{ auth: {}, apiMessage: '...' } },
+  data() { 
+    return {
+      auth: {}, 
+      apiMessage_get: '...', 
+      apiMessage_set: '...',
+      profile: {
+        _id: '',
+        name: '',
+        age: '',
+        haircolor: ''
+      }
+    }
+  },
+  watch: {
+    auth: function(newauth,oldauth){
+      console.log('got to watch');
+      if( newauth && newauth.isAuthenticated ){
+        console.log('got to watch 2');
+        this.getProfile();
+      }
+    }
+  },
   methods: {
-    async callApi(){
+    async getProfile(){
       const token = await this.auth.getTokenSilently();
-      console.log('token: '+token);
-
       // Use Axios to make a call to the API
-      const { data } = await axios.get("http://localhost:3001/api/private", {
+      const { data } = await axios.get(process.env.GRIDSOME_API_URL+"/user/profile", {
         headers: {
-          Authorization: `Bearer ${token}`    // send the access token through the 'Authorization' header
+          Authorization: `Bearer ${token}`
         }
       });
-
-      this.apiMessage = data;
+      this.profile = data;
+    },
+    async setProfile(){
+      const token = await this.auth.getTokenSilently();
+      // Use Axios to make a call to the API
+      const { data } = await axios.post(process.env.GRIDSOME_API_URL+"/user/profile", 
+        this.profile,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
     }
   }
 }
